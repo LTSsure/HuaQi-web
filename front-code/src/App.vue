7d2877ebae85c9e2c4d5a5be161c35eda4dd2b37
chenZiHong-Gavin <191250014@smail.nju.edu.cn>
2020-10-02 14:25:05 +0800

    chore: 增加页面设计目录

--- front-code/src/App.vue
+++ front-code/src/App.vue
@@ -1,13 +1,24 @@
+//综上所述，使用创建好的组件的步骤为：
+//1、import引入组件
+//2、在components中挂载组建
+//3、在template模板中使用组件
+
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <v-hello></v-hello>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Hello from './view/test.vue'
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+    'v-hello':Hello
+  }
+
 }
 </script>
 
---
+++ front-code/src/view/test.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- 所有的内容要被根节点包含起来 -->
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+ 
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'to Your Vue.js App'
+    }
+  }
+}
+</script>
