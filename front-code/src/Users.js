64efc9ca95526bde40773c2f190557496cf1dfd2
LTSure <60334846+LTSsure@users.noreply.github.com>
2020-10-04 23:52:54 +0800

    feat:登录api接口

---
+++ front-code/src/Users.js
@@ -0,0 +1,18 @@
+import { axios } from '@/utils/request'
+const api = {
+    userPre: '/api/Users'
+}
+export function loginAPI(data){
+    return axios({
+        url:`${api.userPre}/login`,
+        method: 'POST',
+        data
+    })
+}
+export function registerAPI(data){
+    return axios({
+        url: `${api.userPre}/register`,
+        method: 'POST',
+        data
+    })
+}
